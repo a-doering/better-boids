dec398bd1eaa2f0b64e3a45cc7934300fedda797
jack89roberts <jroberts@turing.ac.uk>
2022-01-28 14:10:05 +0000

    Use PEP8 naming conventions

--- test_boids.py
+++ test_boids.py
@@ -1,7 +1,7 @@
 import os
 import yaml
 from nose.tools import assert_almost_equal
-from boids import updateBoids
+from boids import update_boids
 
 
 def test_bad_boids_regression():
@@ -9,7 +9,7 @@ def test_bad_boids_regression():
         regression_data = yaml.safe_load(fixture_file)
 
     boid_data = regression_data["before"]
-    updateBoids(boid_data)
+    update_boids(boid_data)
     for after, before in zip(regression_data["after"], boid_data):
         for after_value, before_value in zip(after, before):
             assert_almost_equal(after_value, before_value, delta=0.01)
