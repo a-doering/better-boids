3ed2b34bc09bb7b25daf82519704ed83d7ce8f16
jack89roberts <jroberts@turing.ac.uk>
2022-01-28 16:34:53 +0000

    Refactor Boids structure into a class

--- test_boids.py
+++ test_boids.py
@@ -1,48 +1,46 @@
 import os
 import yaml
 from nose.tools import assert_almost_equal, assert_equal, assert_greater, assert_less
-from boids import get_flock_parameters, initialise_boids, update_boids
+from boids import Boids
 
 
 def test_bad_boids_regression():
     with open(os.path.join(os.path.dirname(__file__), "fixture.yml")) as fixture_file:
         regression_data = yaml.safe_load(fixture_file)
-
-    boid_data = regression_data["before"]
     boid_count = 50
-    flock_parameters = get_flock_parameters(boid_count)
-    update_boids(
-        boid_data,
-        flock_parameters,
-    )
-    for after, before in zip(regression_data["after"], boid_data):
+    boids = Boids.with_default_parameters(boid_count)
+    boids.initialise_from_data(regression_data["before"])
+    boids.update()
+    current_data = (boids.xs, boids.ys, boids.xvs, boids.yvs)
+    for after, before in zip(regression_data["after"], current_data):
         for after_value, before_value in zip(after, before):
             assert_almost_equal(after_value, before_value, delta=0.01)
 
 
 def test_bad_boids_initialisation():
-    boid_count = 50
+    boid_count = 15
+    boids = Boids.with_default_parameters(boid_count)
     x_range = (-450, 50.0)
     y_range = (300.0, 600.0)
     xv_range = (0, 10.0)
     yv_range = (-20.0, 20.0)
-    xs, ys, xvs, yvs = initialise_boids(
+    boids.initialise_random(
         boid_count,
         x_range=x_range,
         y_range=y_range,
         xv_range=xv_range,
         yv_range=yv_range,
     )
-    assert_equal(len(xs), boid_count)
-    for x in xs:
+    assert_equal(len(boids.xs), boid_count)
+    for x in boids.xs:
         assert_less(x, x_range[1])
         assert_greater(x, x_range[0])
-    for y in ys:
+    for y in boids.ys:
         assert_less(y, y_range[1])
         assert_greater(y, y_range[0])
-    for xv in xvs:
+    for xv in boids.xvs:
         assert_less(xv, xv_range[1])
         assert_greater(xv, xv_range[0])
-    for yv in yvs:
+    for yv in boids.yvs:
         assert_less(yv, yv_range[1])
         assert_greater(yv, yv_range[0])
